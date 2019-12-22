47093a450f7c162218d9634f554c49f61d9d73f8
sambell <bell.sam@Outlook.com>
2019-12-21 20:53:03 -0600

    store module

---
+++ store/todos.js
@@ -0,0 +1,26 @@
+const state = () => ({
+    priorities: [
+        { text: "High", value: "high" },
+        { text: "Medium", value: "medium" },
+        { text: "Low", value: "low" }
+      ]
+});
+
+const getters = {
+
+};
+
+const actions = {
+    
+};
+
+const mutations = {
+   
+};
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+};
